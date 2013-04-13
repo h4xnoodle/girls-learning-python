547be20390d3ccc23de627a525e33a24374249f8
Rebecca <h4xnoodle@gmail.com>
2013-04-13 15:41:17 -0400

    Exercises updated

---
+++ exercises/my_turtle.py
@@ -0,0 +1,16 @@
+import turtle
+import os
+
+# Change the turtle to a real turtle shape and set the colour to green.
+turtle.shape('turtle')
+turtle.pensize(5)
+turtle.color([0,0.9,0.1])
+turtle.pencolor("gray25")
+
+# Face her forward
+turtle.left(90)
+
+# After this, we can run commands in iPython interactively.
+
+os.system('clear')
+print "\n\n\nTry moving your Turtle forward!"
